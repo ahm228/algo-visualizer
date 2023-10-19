66434108eaf5c6f29864345f53664867b1a8ee45
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-19 16:12:24 -0500

    Added a few more algos and buttons for them
    Note: drawing not yet added for new algorithms, buttons are present however

--- algoVisualizer.py
+++ algoVisualizer.py
@@ -229,6 +229,111 @@ def bogoSort(array):
         drawArray(array, range(len(array)))
         CLOCK.tick(200)
 
+def bucketSort(array):
+    arr = []
+    slotNum = 10
+    for i in range(slotNum):
+        arr.append([])
+
+    for j in array:
+        indexB = int(slotNum * j)
+        arr[indexB].append(j)
+
+    for i in range(slotNum):
+        arr[i] = insertionSort(arr[i])
+
+    k = 0
+    for i in range(slotNum):
+        for j in range (len(arr[i])):
+            array[k] = arr[i][j]
+            k += 1
+    return array
+
+def countingSort(array):
+    max = max(array)
+
+    countArray = [0] * (max + 1)
+
+    for num in array:
+        countArray[num] += 1
+    
+    for i in range(1, max + 1):
+        countArray[i] += countArray[i - 1]
+
+    outputArray = [0] * len(array)
+
+    for i in range(len(array) - 1, -1, -1):
+        outputArray[countArray[array[i]] - 1] = array
+        countArray[array[i]] -= 1
+    
+    return outputArray
+
+def cocktailShaker(array):
+    n = len(array)
+    swapped = True
+    start = 0
+    end = n - 1
+    while (swapped == True):
+        swapped = False
+        for i in range(start, end):
+            if (array[i] > array[i + 1]):
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
+
+        if (swapped == False):
+            break
+            
+        swapped = False
+
+        end = end - 1
+
+        for i in range(end - 1, start - 1, -1):
+            if (array[i] > array[i + 1]):
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
+
+        start = start + 1
+
+def getNextGap(gap):
+    gap = (gap * 10)/13
+    if gap < 1:
+        return 1
+    return gap
+
+def combSort(array):
+    n = len(array)
+    gap = n
+    swapped = True
+    while gap != 1 or swapped == 1:
+        gap = getNextGap(gap)
+        swapped = False
+        for i in range(0, n - gap):
+            if array[i] > array[i + gap]:
+                array[i], array[i + gap] = array[i + gap], array[i]
+                swapped = True
+
+'''
+def cycleSort(array):
+
+def pancakeSort(array):
+
+def introSort(array):
+
+def bitonicSort(array):
+
+def spaghettiSort(array):
+
+def timSort(array):
+
+def treeSort(array):
+
+def cubeSort(array):
+
+def gnomeSort(array):
+
+def stoogeSort(array):
+'''
+
 def regenerateArray():
     global array
     array = [random.randint(10, HEIGHT - 10) for _ in range(ARRAY_SIZE)]
@@ -246,12 +351,16 @@ BUTTONS = [
     {"label": "Radix Sort", "function": radixSort},
     {"label": "Shell Sort", "function": shellSort},
     {"label": "Bogo Sort", "function": bogoSort},
+    {"label": "Bucket Sort", "function": bucketSort},
+    {"label": "Counting Sort", "function": countingSort},
+    {"label": "Cocktail Shaker Sort", "function": cocktailShaker},
+    {"label": "Comb Sort", "function": combSort},
 ]
 
 BUTTONS.append({"label": "Restart", "function": "RESTART"})
 
 FONT = pygame.font.SysFont("Arial", 20)
-BUTTON_WIDTH, BUTTON_HEIGHT = 200, 40
+BUTTON_WIDTH, BUTTON_HEIGHT = 150, 30
 
 TOTAL_BUTTONS_HEIGHT = len(BUTTONS) * (BUTTON_HEIGHT + 10)
 START_X, START_Y = 10, HEIGHT - TOTAL_BUTTONS_HEIGHT
